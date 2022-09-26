ca1a3e3c5513f681d6ff344f6e52f8626c929d03
codewithdary <info@darynazar.com>
2022-09-26 13:51:54 +0200

    [EP 1] Introduction, Installation & Configuration

---
+++ app/Providers/NovaServiceProvider.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Nova;
+use Laravel\Nova\NovaApplicationServiceProvider;
+
+class NovaServiceProvider extends NovaApplicationServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        // Hide Nova's light/dark mode switcher
+        // Nova::withoutNotificationCenter();
+
+        $this->getFooterContent();
+    }
+
+    /**
+     * Register the Nova routes.
+     *
+     * @return void
+     */
+    protected function routes()
+    {
+        Nova::routes()
+                ->withAuthenticationRoutes()
+                ->withPasswordResetRoutes()
+                ->register();
+    }
+
+    /**
+     * Register the Nova gate.
+     *
+     * This gate determines who can access Nova in non-local environments.
+     *
+     * @return void
+     */
+    protected function gate()
+    {
+        Gate::define('viewNova', function ($user) {
+            return in_array($user->email, [
+                //
+            ]);
+        });
+    }
+
+    /**
+     * Get the dashboards that should be listed in the Nova sidebar.
+     *
+     * @return array
+     */
+    protected function dashboards()
+    {
+        return [
+            new \App\Nova\Dashboards\Main,
+        ];
+    }
+
+    /**
+     * Get the tools that should be listed in the Nova sidebar.
+     *
+     * @return array
+     */
+    public function tools()
+    {
+        return [];
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    private function getFooterContent(): void
+    {
+        Nova::footer(function ($request) {
+            return Blade::render('nova/footer');
+        });
+    }
+}
---
+++ resources/views/nova/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="text-center">
+    <p>
+        This Product Analytics Dashboard has been powered by
+        <a
+            href="https://blog.codewithdary.com"
+            class="text-yellow-600 hover:text-yellow-500 transition-all font-bold">
+            Code With Dary</a>. Feel free to support him.
+    </p>
+</div>
